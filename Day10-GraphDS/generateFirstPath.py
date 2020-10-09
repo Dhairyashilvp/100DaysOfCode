f63f0724b82c3d05a7c986c0a882beb3bea35dda
dhairyashilp <dh.patil09@gmail.com>
2020-10-10 01:29:45 +0530

    Day10-GraphDS

---
+++ Day10-GraphDS/generateFirstPath.py
@@ -0,0 +1,24 @@
+# Program to generate the first path of the graph from the nodes provided
+graph = {
+    'a': ['c'],
+    'b': ['d'],
+    'c': ['e'],
+    'd': ['a', 'd'],
+    'e': ['b', 'c']
+}
+def findPath(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    for node in graph[start]:
+        newPath = findPath(graph, node, end, path)
+        if newPath:
+            return newPath
+        else: 
+            return None
+#function call to print the path
+print(findPath(graph, 'd', 'c'))
+
+            
+        
+
