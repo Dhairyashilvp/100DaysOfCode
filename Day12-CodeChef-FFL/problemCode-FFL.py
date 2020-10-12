fe5ff88276ec5cf9a6d308990687d3da5a47052b
dhairyashilp <dh.patil09@gmail.com>
2020-10-12 23:24:38 +0530

    Day12-CodeChef-FFL

--- Day12-CodeChef-FFL/problemCode-FFL.py
+++ Day12-CodeChef-FFL/problemCode-FFL.py
@@ -2,5 +2,28 @@
 T = int(input())
 
 while T > 0 :
+    c = 0
+    d = []
+    f = []
+    flag = 0
+    ip = input()
+    N,S = ip.split()
+    mm = 100 - int(S)
+    
+    ip1 = input()
+    n1 = ip1.split()
+
+    ip2 = input()
+    n2 = ip2.split()
+
+    for v in n2:
+        if int(v) == 0:
+            d.append(n1[c])
+            c += 1
+        elif int(v) == 1:
+            f.append(n1[c])
+            c += 1
+        else:
+            pass
     
     T -= 1
