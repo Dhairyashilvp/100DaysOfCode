052399314d91f537beb84180387932c0b3124ae1
dhairyashilp <dh.patil09@gmail.com>
2020-10-13 23:24:08 +0530

    Day13-CodeChef-COINS

--- Day13-CodeChef-COINS/problemCode-COINS.py
+++ Day13-CodeChef-COINS/problemCode-COINS.py
@@ -6,3 +6,9 @@ def process(n):
     else:
         dic[n] = max(n, process(n//2) + process(n//3) + process(n//4))
         return dic[n]
+while True:
+    try:
+        n = int(input())
+        print(process(n))
+    except:
+        break
