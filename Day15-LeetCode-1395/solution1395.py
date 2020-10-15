c790b25e4eae99d29f038f2d3e0edb21fe1b64db
dhairyashilp <dh.patil09@gmail.com>
2020-10-15 23:50:45 +0530

    Day15-LeetCode-1395

--- Day15-LeetCode-1395/solution1395.py
+++ Day15-LeetCode-1395/solution1395.py
@@ -1,3 +1,10 @@
 class Solution:
     def numTeams(self, rating: List[int]) -> int:
-        
+        n = len(rating)
+        ans = []
+        for i in range(n-2):
+            for j in range(i + 1,n-1):
+                for k in range(j + 1,n):
+                    if (rating[i] < rating[j] and rating[j] < rating[k]) or (rating[i] > rating[j] and rating[j] > rating[k]) :
+                        ans.append((rating[i], rating[j], rating[k]))
+        return len(ans)
