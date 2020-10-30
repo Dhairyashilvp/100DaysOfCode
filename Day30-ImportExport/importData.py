abf504aba7645a66bb244f3530be0d33da21e757
dhairyashilp <dh.patil09@gmail.com>
2020-10-30 23:52:49 +0530

    Day30-ImportExport

--- Day30-ImportExport/importData.py
+++ Day30-ImportExport/importData.py
@@ -6,3 +6,5 @@ import matplotlib.pyplot as plotter
 df = xlrd.open_workbook('trialData.xlsx')
 sheet = df.sheet_by_name('Sheet1')
 
+rdata = [[sheet.cell_value(r, c) for c in range(sheet.ncols)] for r in range(sheet.nrows)]
+
