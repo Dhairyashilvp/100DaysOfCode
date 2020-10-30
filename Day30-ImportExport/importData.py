91f8f7a16bc2437fa63217d17f1405ff9e78cfb4
dhairyashilp <dh.patil09@gmail.com>
2020-10-30 23:53:14 +0530

    Day30-ImportExport

--- Day30-ImportExport/importData.py
+++ Day30-ImportExport/importData.py
@@ -8,3 +8,27 @@ sheet = df.sheet_by_name('Sheet1')
 
 rdata = [[sheet.cell_value(r, c) for c in range(sheet.ncols)] for r in range(sheet.nrows)]
 
+rdata = [[sheet.cell_value(r, c) for c in range(sheet.ncols)] for r in range(sheet.nrows)]
+my_data_dict = {}
+temp_dict = {}
+pieLabels = []
+share = []
+rdata.pop(0)
+last = rdata.pop(-1)
+i = 0
+for rd in rdata:
+    temp_dict.clear()
+    rd.pop(0)
+    if rd[1] != "" or rd[2] != "":
+        my_data_dict.update({rd[0]:rd[2]})
+    else:
+        pass
+for (key, value) in sorted(my_data_dict.items(), key=lambda x: x[1], reverse=True):
+    pieLabels.append(key)
+    share.append(value)
+    if i > 7 :
+        break
+    else:
+        i += 1
+pieLabels.append("Other Countries")
+share.append(100 - sum(share))
