6ff8b44d1d0f2f840e09794c1259e98be0b9c2c6
dhairyashilp <dh.patil09@gmail.com>
2020-11-06 22:46:43 +0530

    Day36-HR-NoIdea

---
+++ Day36-HR-NoIdea/noIdea.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n,m = map(int,input().split())
+nel = list(map(int,input().split()))
+A = set(map(int,input().split()))
+B = set(map(int,input().split()))
+h = 0
+
