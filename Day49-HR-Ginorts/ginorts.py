03bc247a27268e387e5127b4f79f9f93d3f386cc
dhairyashilp <dh.patil09@gmail.com>
2020-11-18 01:17:45 +0530

    Day49-HR-Ginorts

---
+++ Day49-HR-Ginorts/ginorts.py
@@ -0,0 +1,21 @@
+s = list(raw_input())
+enum = []
+onum = []
+cap = []
+sc = []
+for e in s:
+    if e.isdigit():
+        if int(e) % 2 == 0:
+            enum.append(e)
+        else:
+            onum.append(e)
+    elif e.isupper():
+        cap.append(e)
+    else:
+        sc.append(e)
+enum.sort()
+onum.sort()
+cap.sort()
+sc.sort()
+ans = ""
+print(ans.join(sc) + ans.join(cap) + ans.join(onum) + ans.join(enum))
