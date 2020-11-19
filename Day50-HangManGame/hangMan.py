27f2b2f49532f2a8d76d4cc5febe7c43e63ed20b
dhairyashilp <dh.patil09@gmail.com>
2020-11-19 22:21:42 +0530

    Day50-HangManGame

--- Day50-HangManGame/hangMan.py
+++ Day50-HangManGame/hangMan.py
@@ -1,3 +1,14 @@
 import time
 from random_word import RandomWords
 
+name = input("Enter Your name: ")
+
+print("Hello, " + name, "Time to play hangman!")
+time.sleep(1)
+print("Start Guess....")
+time.sleep(0.5)
+r = RandomWords()
+w = r.get_random_words()
+word = w[0]
+guesses = ""
+turns = 10
