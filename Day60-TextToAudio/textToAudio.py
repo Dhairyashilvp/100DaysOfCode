9bc8dcf4eb0511651a6a337fe4b365e64f871d2c
dhairyashilp <dh.patil09@gmail.com>
2020-11-29 21:19:42 +0530

    Day60-TextToAudio

--- Day60-TextToAudio/textToAudio.py
+++ Day60-TextToAudio/textToAudio.py
@@ -1,3 +1,15 @@
+#This will narrate the whole text in the rpogram execution itself
+'''import pyttsx3
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+f = open('book.txt')
+x = f.read().lower()
+speak(x)
+'''
+
+
 #This will read the text and then save it to an audio file in the format mentioned like mp3/wav etc
 from gtts import gTTS
 import os
