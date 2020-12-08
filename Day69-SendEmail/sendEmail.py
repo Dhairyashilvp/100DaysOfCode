a0e589943a9b2e30277941c85b0308c83d578ca9
dhairyashilp <dh.patil09@gmail.com>
2020-12-08 22:51:19 +0530

    Day69-SendEmail

---
+++ Day69-SendEmail/sendEmail.py
@@ -0,0 +1,19 @@
+import smtplib, ssl
+port = 456
+sender = "Your Email"
+password = "Your Password"
+
+recieve = "Reciever's Mail ID"
+
+message = """\
+Subject: Sent via Python
+Hey there stupid
+"""
+context = ssl.create_default_context()
+
+print("Sending process initiated")
+with smtplib.SMTP_SSL("smtp.gmail.com",port,context=context) as server:
+    server.login(sender,password)
+    server.sendmail(sender,recieve,message)
+
+print("Mail sent Successfully")
