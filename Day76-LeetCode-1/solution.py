a7159865e684fe46f1e2abae5edd2c5eda6c2811
dhairyashilp <dh.patil09@gmail.com>
2020-12-29 17:01:43 -0500

    Day77-LeetCode-283

--- Day76-LeetCode-1/solution.py
+++ Day76-LeetCode-1/solution.py
@@ -1,3 +1,9 @@
 class Solution:
-    def numJewelsInStones(self, J, S):
-        return sum(s in J for s in S)
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        buf = {}
+        for i, n in enumerate(nums):
+            s = target - n
+            if s in buf:
+                return [buf[s], i]
+            else:
+                buf[n] = i
---
+++ Day77-LeetCode-283/ReadMe.txt
@@ -0,0 +1,10 @@
+This is my 77th Day of 100Days of code 
+Today I Solver leetcode problem (https://leetcode.com/problems/move-zeroes/):
+
+Referances:
+https://leetcode.com/problems/move-zeroes/
+
+ ---------------------------------------------------
+|to run simply use: python .\solution.py        |
+ ---------------------------------------------------
+:)
