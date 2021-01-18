2a9044d3677b61ebd7720bc8a5f037ca0e64921d
dhairyashilp <dh.patil09@gmail.com>
2021-01-18 14:27:03 -0500

    Day90-PasswordGenerator

---
+++ Day90-PasswordGenerator/passwordGenerator.py
@@ -0,0 +1,14 @@
+import random
+
+while True:
+    numberOfChar = int(input('Entet the Number of charecters you want in your Password : '))
+    numberOfUpper = int(input('What is the least number of UPPERCASE letters needed in your Password? : '))
+    numberOfLower = int(input('What is the least number of lowercase letters needed in your Password? : '))
+    numberOfDigits = int(input('What is the least number of Digits needed in your Password: '))
+    numberOfSym = int(input('What is the least number of Symbols in your Password: '))
+
+    if numberOfChar < numberOfUpper + numberOfLower + numberOfDigits + numberOfSym:
+        print('The number of charecters are lesser then required')
+    else:
+        break
+
