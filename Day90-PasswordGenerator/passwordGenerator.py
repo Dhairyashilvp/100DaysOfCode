4655800a2e4abdefb6e0b239bb7d41e1f7e3f1c5
dhairyashilp <dh.patil09@gmail.com>
2021-01-18 14:41:36 -0500

    Day90-PasswordGenerator

--- Day90-PasswordGenerator/passwordGenerator.py
+++ Day90-PasswordGenerator/passwordGenerator.py
@@ -18,23 +18,5 @@ while True:
     else:
         break
 
-uppercaseList = [chr(i) for i in range(65,91)]
-upper_char = getRandomzedList(uppercaseList,numberOfUpper)
-
-lower_list = [chr(i) for i in range(97,123)]
-lower_char = getRandomzedList(lower_list,numberOfLower)
-
-digit_list = [str(i) for i in range(0,10)]
-digit_char = getRandomzedList(digit_list,numberOfDigits)
-
-sym_list = [chr(i) for i in range(33,48)]
-sym_list +=[chr(i) for i in range (58,65)]
-sym_list +=[chr(i) for i in range (91,97)]
-sym_list +=[chr(i) for i in range (123,127)]
-sym_char = getRandomzedList(sym_list,numberOfSym)
-
-num_blank_char = numberOfChar - numberOfUpper - numberOfLower - numberOfDigits - numberOfSym
-full_list = uppercaseList + lower_list + digit_list + sym_list
-fillers_char = getRandomzedList(full_list,num_blank_char)
 
 
